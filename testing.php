5567fa947f5a683a4d3689fb69c2545a38cf096c
Yehezkiel Rusli <kielrusli07@gmail.com>
2021-05-09 17:51:02 +0700

    fix testing.php

--- testing.php
+++ testing.php
@@ -1,10 +1,18 @@
 <?php
-include("connection.php");
-    $pass = hash("sha256", "admin123321");
-    $insert_penatua = "INSERT INTO user (username, password) VALUES ('admin', '$pass')";
-    if($con->query($insert_penatua)){
+    include("connection.php");
+    // $pass = hash("sha256", "admin123321");
+    // $insert_penatua = "INSERT INTO user (username, password) VALUES ('admin', '$pass')";
+    // if($con->query($insert_penatua)){
+    //     echo "sukses";
+    // }else{
+    //     echo "gagal";
+    // }
+
+    $remove_tamu = "DELETE FROM Tamu";
+    if($con->query($remove_tamu)){
         echo "sukses";
     }else{
         echo "gagal";
     }
 ?> 
+
